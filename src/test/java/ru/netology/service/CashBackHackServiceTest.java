6e7b4feb0c51114e3fc825e9adf193f4c5af9c0e
yakov23 <yakovoberderfer13@gmail.com>
2024-02-28 16:31:40 +0300

    tests added

---
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.service;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class CashBackHackServiceTest {
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testRemainWithCashBack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+}
